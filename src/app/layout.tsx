f11f0664808f16e0fc21eee18141240e10a6556d
Atif Hameed <atifhameed11312@gmail.com>
2024-05-08 23:38:06 +0500

    update code

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+
+const font = DM_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Plura",
+  description: "All in one agency, solution",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider appearance={{ baseTheme: dark }}>
+
+    <html lang="en" suppressHydrationWarning>
+      <body className={font.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+     </ClerkProvider>
+  );
+}
