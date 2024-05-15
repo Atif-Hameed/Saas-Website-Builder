eb8744b15ca086883d3e0c3aa42e8b4219d61879
Sahaab-Tariq <sahabtariq888@gmail.com>
2024-05-15 07:02:08 +0500

    modal create

---
+++ src/components/global/infobar.tsx
@@ -0,0 +1,103 @@
+'use client'
+import { NotificationWithUser } from '@/lib/types'
+import { UserButton } from '@clerk/nextjs'
+import React, { useState } from 'react'
+import { twMerge } from 'tailwind-merge'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '../ui/sheet'
+import { Bell } from 'lucide-react'
+import { Role } from '@prisma/client'
+import { Card } from '../ui/card'
+import { Switch } from '../ui/switch'
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
+import { ModeToggle } from './mode-toggle'
+
+type Props = {
+  notifications: NotificationWithUser | []
+  role?: Role
+  className?: string
+  subAccountId?: string
+}
+
+const InfoBar = ({ notifications, subAccountId, className, role }: Props) => {
+  const [allNotifications, setAllNotifications] = useState(notifications)
+  const [showAll, setShowAll] = useState(true)
+  return (
+    <>
+      <div className="flex items-center gap-2 ml-auto">
+          <UserButton afterSignOutUrl="/" />
+          <Sheet>
+            <SheetTrigger>
+              <div className="rounded-full w-9 h-9 bg-primary flex items-center justify-center text-white">
+                <Bell size={17} />
+              </div>
+            </SheetTrigger>
+            <SheetContent showX className="mt-4 mr-4 pr-4 overflow-scroll">
+              <SheetHeader className="text-left">
+                <SheetTitle>Notifications</SheetTitle>
+                <SheetDescription>
+                  {(role === 'AGENCY_ADMIN' || role === 'AGENCY_OWNER') && (
+                    <Card className="flex items-center justify-between p-4">
+                      Current Subaccount
+                      <Switch />
+                    </Card>
+                  )}
+                </SheetDescription>
+              </SheetHeader>
+              {allNotifications?.map((notification) => (
+                <div
+                  key={notification.id}
+                  className="flex flex-col gap-y-2 mb-2 overflow-x-scroll text-ellipsis"
+                >
+                  <div className="flex gap-2">
+                    <Avatar>
+                      <AvatarImage
+                        src={notification.User.avatarUrl}
+                        alt="Profile Picture"
+                      />
+                      <AvatarFallback className="bg-primary">
+                        {notification.User.name.slice(0, 2).toUpperCase()}
+                      </AvatarFallback>
+                    </Avatar>
+                    <div className="flex flex-col">
+                      <p>
+                        <span className="font-bold">
+                          {notification.notification.split('|')[0]}
+                        </span>
+                        <span className="text-muted-foreground">
+                          {notification.notification.split('|')[1]}
+                        </span>
+                        <span className="font-bold">
+                          {notification.notification.split('|')[2]}
+                        </span>
+                      </p>
+                      <small className="text-xs text-muted-foreground">
+                        {new Date(notification.createdAt).toLocaleDateString()}
+                      </small>
+                    </div>
+                  </div>
+                </div>
+              ))}
+              {allNotifications?.length === 0 && (
+                <div
+                  className="flex items-center justify-center text-muted-foreground"
+                  mb-4
+                >
+                  You have no notifications
+                </div>
+              )}
+            </SheetContent>
+          </Sheet>
+          <ModeToggle />
+        </div>
+    </>
+  )
+}
+
+export default InfoBar
--- src/components/sidebar/menu-options.tsx
+++ src/components/sidebar/menu-options.tsx
@@ -24,18 +24,21 @@ import {
 } from '../ui/command'
 import Link from 'next/link'
 import { twMerge } from 'tailwind-merge'
+import { useModal } from '@/providers/modal-provider'
+import CustomModal from '../global/custom-modal'
+import SubAccountDetails from '../forms/subaccount-details'
 import { Separator } from '../ui/separator'
 import { icons } from '@/lib/constants'
 
 type Props = {
-  defaultOpen?: boolean;
-  subAccounts: SubAccount[];
-  sidebarOpt: AgencySidebarOption[] | SubAccountSidebarOption[];
-  sidebarLogo: string;
-  details: any;
-  user: any;
-  id: string;
-};
+  defaultOpen?: boolean
+  subAccounts: SubAccount[]
+  sidebarOpt: AgencySidebarOption[] | SubAccountSidebarOption[]
+  sidebarLogo: string
+  details: any
+  user: any
+  id: string
+}
 
 const MenuOptions = ({
   details,
@@ -43,10 +46,10 @@ const MenuOptions = ({
   sidebarLogo,
   sidebarOpt,
   subAccounts,
-  defaultOpen,
   user,
+  defaultOpen,
 }: Props) => {
-
+  const { setOpen } = useModal()
   const [isMounted, setIsMounted] = useState(false)
 
   const openState = useMemo(
@@ -61,10 +64,9 @@ const MenuOptions = ({
   if (!isMounted) return
 
   return (
-    <Sheet 
-    modal={false} 
-    open={true}
-    // {...openState}
+    <Sheet
+      modal={false}
+      {...openState}
     >
       <SheetTrigger
         asChild
@@ -77,6 +79,7 @@ const MenuOptions = ({
           <Menu />
         </Button>
       </SheetTrigger>
+
       <SheetContent
         showX={!defaultOpen}
         side={'left'}
@@ -88,8 +91,8 @@ const MenuOptions = ({
           }
         )}
       >
-      <div>
-      <AspectRatio ratio={16 / 5}>
+        <div>
+          <AspectRatio ratio={16 / 5}>
             <Image
               src={sidebarLogo}
               alt="Sidebar Logo"
@@ -98,12 +101,12 @@ const MenuOptions = ({
             />
           </AspectRatio>
           <Popover>
-          <PopoverTrigger asChild>
+            <PopoverTrigger asChild>
               <Button
                 className="w-full my-4 flex items-center justify-between py-8"
                 variant="ghost"
               >
-              <div className="flex items-center text-left gap-2">
+                <div className="flex items-center text-left gap-2">
                   <Compass />
                   <div className="flex flex-col">
                     {details.name}
@@ -121,11 +124,11 @@ const MenuOptions = ({
               </Button>
             </PopoverTrigger>
             <PopoverContent className="w-80 h-80 mt-4 z-[200]">
-              <Command>
+              <Command className="rounded-lg">
                 <CommandInput placeholder="Search Accounts..." />
-                <CommandList  className="pb-16">
-                <CommandEmpty> No results found</CommandEmpty>
-                                  {(user?.role === 'AGENCY_OWNER' ||
+                <CommandList className="pb-16">
+                  <CommandEmpty> No results found</CommandEmpty>
+                  {(user?.role === 'AGENCY_OWNER' ||
                     user?.role === 'AGENCY_ADMIN') &&
                     user?.Agency && (
                       <CommandGroup heading="Agency">
@@ -176,15 +179,125 @@ const MenuOptions = ({
                         </CommandItem>
                       </CommandGroup>
                     )}
-
+                  <CommandGroup heading="Accounts">
+                    {!!subAccounts
+                      ? subAccounts.map((subaccount) => (
+                          <CommandItem key={subaccount.id}>
+                            {defaultOpen ? (
+                              <Link
+                                href={`/subaccount/${subaccount.id}`}
+                                className="flex gap-4 w-full h-full"
+                              >
+                                <div className="relative w-16">
+                                  <Image
+                                    src={subaccount.subAccountLogo}
+                                    alt="subaccount Logo"
+                                    fill
+                                    className="rounded-md object-contain"
+                                  />
+                                </div>
+                                <div className="flex flex-col flex-1">
+                                  {subaccount.name}
+                                  <span className="text-muted-foreground">
+                                    {subaccount.address}
+                                  </span>
+                                </div>
+                              </Link>
+                            ) : (
+                              <SheetClose asChild>
+                                <Link
+                                  href={`/subaccount/${subaccount.id}`}
+                                  className="flex gap-4 w-full h-full"
+                                >
+                                  <div className="relative w-16">
+                                    <Image
+                                      src={subaccount.subAccountLogo}
+                                      alt="subaccount Logo"
+                                      fill
+                                      className="rounded-md object-contain"
+                                    />
+                                  </div>
+                                  <div className="flex flex-col flex-1">
+                                    {subaccount.name}
+                                    <span className="text-muted-foreground">
+                                      {subaccount.address}
+                                    </span>
+                                  </div>
+                                </Link>
+                              </SheetClose>
+                            )}
+                          </CommandItem>
+                        ))
+                      : 'No Accounts'}
+                  </CommandGroup>
                 </CommandList>
+                {(user?.role === 'AGENCY_OWNER' ||
+                  user?.role === 'AGENCY_ADMIN') && (
+                  <SheetClose>
+                    <Button
+                      className="w-full flex gap-2"
+                      onClick={() => {
+                        setOpen(
+                          <CustomModal
+                            title="Create A Subaccount"
+                            subheading="You can switch between your agency account and the subaccount from the sidebar"
+                          >
+                            <SubAccountDetails
+                              agencyDetails={user?.Agency as Agency}
+                              userId={user?.id as string}
+                              userName={user?.name}
+                            />
+                          </CustomModal>
+                        )
+                      }}
+                    >
+                      <PlusCircleIcon size={15} />
+                      Create Sub Account
+                    </Button>
+                  </SheetClose>
+                )}
               </Command>
             </PopoverContent>
           </Popover>
-      </div>
+          <p className="text-muted-foreground text-xs mb-2">MENU LINKS</p>
+          <Separator className="mb-4" />
+          <nav className="relative">
+            <Command className="rounded-lg overflow-visible bg-transparent">
+              <CommandInput placeholder="Search..." />
+              <CommandList className="py-4 overflow-visible">
+                <CommandEmpty>No Results Found</CommandEmpty>
+                <CommandGroup className="overflow-visible">
+                  {sidebarOpt.map((sidebarOptions) => {
+                    let val
+                    const result = icons.find(
+                      (icon) => icon.value === sidebarOptions.icon
+                    )
+                    if (result) {
+                      val = <result.path />
+                    }
+                    return (
+                      <CommandItem
+                        key={sidebarOptions.id}
+                        className="md:w-[320px] w-full"
+                      >
+                        <Link
+                          href={sidebarOptions.link}
+                          className="flex items-center gap-2 hover:bg-transparent rounded-md transition-all md:w-full w-[320px]"
+                        >
+                          {val}
+                          <span>{sidebarOptions.name}</span>
+                        </Link>
+                      </CommandItem>
+                    )
+                  })}
+                </CommandGroup>
+              </CommandList>
+            </Command>
+          </nav>
+        </div>
       </SheetContent>
     </Sheet>
   )
-};
+}
 
-export default MenuOptions;
+export default MenuOptions
