68267ac0e94631fe3b1b13cf43f823679fd039ab
Sahaab-Tariq <sahabtariq888@gmail.com>
2024-05-19 09:39:11 +0500

    media storage for subaccounts

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -12,6 +12,7 @@ import {
 } from '@prisma/client'
 
 import { v4 } from 'uuid'
+import { CreateMediaType } from './types'
 
 export const getAuthUserDetails = async () => {
   const user = await currentUser()
@@ -488,4 +489,38 @@ export const sendInvitation = async (
   }
 
   return resposne
+}
+
+export const getMedia = async (subaccountId: string) => {
+  const mediafiles = await db.subAccount.findUnique({
+    where: {
+      id: subaccountId,
+    },
+    include: { Media: true },
+  })
+  return mediafiles
+}
+
+export const createMedia = async (
+  subaccountId: string,
+  mediaFile: CreateMediaType
+) => {
+  const response = await db.media.create({
+    data: {
+      link: mediaFile.link,
+      name: mediaFile.name,
+      subAccountId: subaccountId,
+    },
+  })
+
+  return response
+}
+
+export const deleteMedia = async (mediaId: string) => {
+  const response = await db.media.delete({
+    where: {
+      id: mediaId,
+    },
+  })
+  return response
 }
