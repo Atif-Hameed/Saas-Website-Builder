ce6a76cf233cb0541d914c5cd5e8b84c3ecae1cb
Sahaab-Tariq <sahabtariq888@gmail.com>
2024-05-30 07:18:42 +0500

    stripe sections pending but complete the funnel section and continue the builder section

---
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/_components/funnel-settings.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+import { Funnel } from '@prisma/client'
+import { db } from '@/lib/db'
+
+import FunnelForm from '@/components/forms/funnel-form'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+interface FunnelSettingsProps {
+  subaccountId: string
+  defaultData: Funnel
+}
+
+const FunnelSettings: React.FC<FunnelSettingsProps> = async ({
+  subaccountId,
+  defaultData,
+}) => {
+  const subaccountDetails = await db.subAccount.findUnique({
+    where: {
+      id: subaccountId,
+    },
+  })
+
+  if (!subaccountDetails) return
+
+  return (
+    <div className="flex gap-4 flex-col xl:!flex-row">
+      <Card className="flex-1 flex-shrink">
+        <CardHeader>
+          <CardTitle>Funnel Products</CardTitle>
+          <CardDescription>
+            Select the products and services you wish to sell on this funnel.
+            You can sell one time and recurring products too.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <h1>Pending stripe</h1>
+        </CardContent>
+      </Card>
+
+      <FunnelForm
+        subAccountId={subaccountId}
+        defaultData={defaultData}
+      />
+    </div>
+  )
+}
+
+export default FunnelSettings
