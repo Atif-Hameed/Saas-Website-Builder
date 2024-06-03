95f4a4588bd446f1cc1571daae5bd7985cb9d5ef
Sahaab-Tariq <sahabtariq888@gmail.com>
2024-06-03 23:00:44 +0500

    stripe error

--- src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/_components/funnel-step-card.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/_components/funnel-step-card.tsx
@@ -1,3 +1,4 @@
+
 import { Card, CardContent } from '@/components/ui/card'
 import { FunnelPage } from '@prisma/client'
 import { ArrowDown, Mail } from 'lucide-react'
--- src/app/api/stripe/create-customer/route.ts
+++ src/app/api/stripe/create-customer/route.ts
@@ -1,32 +1,25 @@
-import { stripe } from '@/lib/stripe';
-import { StripeCustomerType } from '@/lib/types';
-import { NextResponse } from 'next/server';
+import { stripe } from '@/lib/stripe'
+import { StripeCustomerType } from '@/lib/types'
+import { NextResponse } from 'next/server'
 
 export async function POST(req: Request) {
+  const { address, email, name, shipping }: StripeCustomerType =
+    await req.json()
+
+  if (!email || !address || !name || !shipping)
+    return new NextResponse('Missing data', {
+      status: 400,
+    })
   try {
-    const { address, email, name, shipping }: StripeCustomerType = await req.json();
-
-    // Log incoming data
-    console.log('Incoming data:', { address, email, name, shipping });
-
-    // Validate required fields
-    if (!email || !address || !name || !shipping) {
-      console.log('Validation failed:', { email, address, name, shipping });
-      return new NextResponse('Missing data', { status: 400 });
-    }
-
     const customer = await stripe.customers.create({
       email,
       name,
       address,
       shipping,
-    });
-
-    console.log('Created customer:', customer);
-
-    return NextResponse.json({ customerId: customer.id });
+    })
+    return Response.json({ customerId: customer.id })
   } catch (error) {
-    console.error('🔴 Error', error);
-    return new NextResponse('Internal Server Error', { status: 500 });
+    console.log('🔴 Error', error)
+    return new NextResponse('Internal Server Error', { status: 500 })
   }
 }
